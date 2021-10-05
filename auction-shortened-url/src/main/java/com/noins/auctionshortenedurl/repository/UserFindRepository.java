5fd5143ca2eea127d5d9fe323d1bcc0ddc293fc0
oguzhanztrk <44616866+oguzhanztrk@users.noreply.github.com>
2021-10-05 22:06:09 +0300

    Delete auction-shortened-url directory

--- auction-shortened-url/src/main/java/com/noins/auctionshortenedurl/repository/UserFindRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noins.auctionshortenedurl.repository;
-
-import com.noins.auctionshortenedurl.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserFindRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
-
-}
